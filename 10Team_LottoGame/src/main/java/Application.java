527ee1fe4db6140367f84f5435e2a71ad09666e4
Ho-Da-Dak <100784510+subsub97@users.noreply.github.com>
2023-12-01 20:19:55 +0900

    refactor(Application) : Jframe코드 삭제 , System.exit 추가
    로또게임 작동간 연관이 없는 JFrame 코드를 삭제하였고
    exit() 메서드를 사용하지 않아 종료되지 않던 문제를 해결

--- 10Team_LottoGame/src/main/java/Application.java
+++ 10Team_LottoGame/src/main/java/Application.java
@@ -1,7 +1,6 @@
 import View.GUIInputView;
 import View.InputView;
 import controller.Controller;
-import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import lotto.LottoMachine;
 
@@ -9,21 +8,14 @@ public class Application {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             createAndShowGUI();
+            System.exit(0);
         });
     }
 
     private static void createAndShowGUI() {
-        JFrame frame = new JFrame("Lotto Application");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         InputView inputView = new GUIInputView();
         LottoMachine lottoMachine = new LottoMachine();
-
         Controller controller = new Controller(lottoMachine, inputView);
-
         controller.run();
-
-        frame.pack();
-        frame.setVisible(true);
     }
 }
